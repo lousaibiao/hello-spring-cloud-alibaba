9fda5339ef9afd0491a64df58674f902efddfd26
lousaibiao <lousaibiao@live.cn>
2019-09-12 10:36:36 +0800

    add provider

---
+++ hello-spring-cloud-alibaba-provider/src/main/java/com/lou/hello/spring/cloud/alibaba/provider/HelloSpringCloudAlibabaProviderApplication.java
@@ -0,0 +1,13 @@
+package com.lou.hello.spring.cloud.alibaba.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringCloudAlibabaProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudAlibabaProviderApplication.class, args);
+    }
+
+}
